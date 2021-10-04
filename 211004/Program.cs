9c2995bd3b503312b9501f3a28185727156789dd
Werenyckyj Vojtěch <werenyckyj.vo.2019@skola.ssps.cz>
2021-10-04 13:00:20 +0200

    Přidejte soubory projektu.

---
+++ 211004/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _211004
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            foreach (string arg in args)
+            {
+                Console.Write(arg + " ");
+            }
+            Console.ReadLine();
+        }
+    }
+}
